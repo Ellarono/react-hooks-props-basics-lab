a6e93be4b4b0cf7be32fe84837f3705c50f4ddbd
Daniella Rono <daniellarono@gmail.com>
2024-09-16 01:32:53 +0300

    all tests passing

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "../components/Links.js"; // Import the Links component
 
-function About() {
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {/* Conditionally render the bio only if it's provided */}
+      {bio && <p>{bio}</p>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* Render the Links component and pass down the links props */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
